d35a495b734be26c708877a3620e4126675de98f
Adrian <X00194620@mytudublin>
2024-01-26 16:22:27 +0000

    Templates Views and Urls setup. Home page working

--- CustomGuitars/CustomGuitars/settings.py
+++ CustomGuitars/CustomGuitars/settings.py
@@ -116,7 +116,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- CustomGuitars/shop/urls.py
+++ CustomGuitars/shop/urls.py
@@ -4,7 +4,4 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.index, name = 'home'),
-    path('<uuid:category_id>/', views.prod_list, name = 'products_by_category'),
-    path('<uuid:category_id>/<uuid:product_id>', views.product_detail, name = 'product_detail'),
-    path('bestsellers/', views.bestseller, name = 'bestsellers'),
 ]
