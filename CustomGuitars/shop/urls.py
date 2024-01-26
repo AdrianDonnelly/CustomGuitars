4a8c1c56bbe197142ec1e649a8b6bea44e233b9d
Adrian <X00194620@mytudublin>
2024-01-26 17:19:33 +0000

    Added categories page and button to the nav bar to use it

--- CustomGuitars/shop/urls.py
+++ CustomGuitars/shop/urls.py
@@ -4,4 +4,7 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.index, name = 'home'),
+    path('<uuid:category_id>/', views.prod_list, name = 'products_by_category'),
+    path('<uuid:category_id>/<uuid:product_id>', views.product_detail, name = 'product_detail'),
+    
 ]
