724ba8ef1fd4093953664c26a172847d99b085b6
Adrian Donnelly <X00194620@mytudublin.ie>
2024-03-29 23:56:16 +0000

    Fixed category image

--- CustomGuitars/shop/urls.py
+++ CustomGuitars/shop/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('add/<uuid:product_id>/', views.add_compare, name='add_compare'),
     path('remove/<uuid:product_id>/',views.compare_remove, name='compare_remove'),
     path('view_images/<uuid:product_id>/', views.view_images, name='view_images'),
-
     path('wishlist/', views.wishlist_detail, name='wishlist_detail'),
     path('wishlist/add/<uuid:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
     path('wishlist/remove/<uuid:product_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
