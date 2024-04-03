d73a645c53affc00c1f0f28dbf660362ab8f5b39
Adrian Donnelly <X00194620@mytudublin.ie>
2024-04-03 16:10:11 +0100

    Whatsapp and sms for orders

--- CustomGuitars/order/views.py
+++ CustomGuitars/order/views.py
@@ -6,6 +6,7 @@ from django.core.mail import send_mail
 from django.shortcuts import render
 from order.models import Order
 from django.conf import settings
+from twilio.rest import Client
 
 
 
@@ -21,12 +22,34 @@ def sender_order_email(order):
         message += f"Product: {item.product}, Quantity: {item.quantity}\n"
         
     send_mail(subject, message, settings.EMAIL_HOST_USER, [recipient],fail_silently=False)
+    
+def whatsapp_order(order):
+    account_sid = "AC35862d2a87f33839b12634c64f4e1e9b"
+    auth_token  = "56d28fbe8fd7bd44450093c716229d82"
+    
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        to="whatsapp:+353851876788",
+        from_="whatsapp:+14155238886",
+        body=f"Thanks for your order!\n\nOrder Number: {order.id}\nTotal: {order.total}\n\n")
 
+def sms_order(order):
+    account_sid = "AC35862d2a87f33839b12634c64f4e1e9b"
+    auth_token  = "56d28fbe8fd7bd44450093c716229d82"
+    
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        to="+353851765118",
+        from_="+12058289417",
+        body=f"Thanks for your order!\n\nOrder Number: {order.id}\nTotal: {order.total}\n\n")
+   
 def thanks(request, order_id):
     customer_order = get_object_or_404(Order, id=order_id)
     
     
     sender_order_email(customer_order)
+    whatsapp_order(customer_order)
+    sms_order(customer_order)
     
     return render(request, 'thanks.html', {'customer_order': customer_order})
 
