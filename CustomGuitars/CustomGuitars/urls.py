277a62d14e11c8281c026cf9ec09455c8fcc441b
Adrian Donnelly <X00194620@mytudublin.ie>
2024-04-15 10:56:28 +0100

    updated sidebar

--- CustomGuitars/CustomGuitars/urls.py
+++ CustomGuitars/CustomGuitars/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls,name="admin"),
     path('',include('shop.urls')),
     path('',include('configurator.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
